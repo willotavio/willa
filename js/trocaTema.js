b6259dcce5c8d9bb9137f15229112f80a7956710
willotavio <willaotavio@gmail.com>
2022-10-29 16:16:47 -0300

    Ajustes no botão de troca de tema

--- js/trocaTema.js
+++ js/trocaTema.js
@@ -1,8 +1,15 @@
 const trocaTema = document.querySelector(".trocaTema");
-/* let light = document.querySelector('.light'); */
+var light = document.getElementsByClassName('light');
 
 trocaTema.addEventListener("click", function(){
-    document.getElementsByClassName('light')[0].classList.toggle("dark");
-    document.getElementsByClassName('light')[1].classList.toggle("dark");
-    document.getElementsByClassName('light')[2].classList.toggle("dark");
+    for( var i = 0; i < light.length; i++){
+        document.getElementsByClassName('light')[i].classList.toggle("dark");
+    }
+    var className = document.body.className;
+    if(className == "light") {
+        this.textContent = "Light";
+    }
+    else {
+        this.textContent = "Dark";
+    }
 })
