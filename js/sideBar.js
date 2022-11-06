63cea417e5fe0fd03d8080ba554d4a6dc3a07ac3
willotavio <willaotavio@gmail.com>
2022-11-06 12:02:45 -0300

    Lista de linguagens adicionada

--- js/sideBar.js
+++ js/sideBar.js
@@ -2,14 +2,12 @@ const botaoSideBar = document.querySelector(".botaoSideBar");
 
 botaoSideBar.addEventListener("click", function(){
   if(botaoSideBar.classList.contains('sideBarFechada')){
-    document.getElementById("conteudoSideBar").style.width = "175px";
-    document.getElementById("corpoSite").style.marginLeft = "175px";
+    document.getElementById("conteudoSideBar").style.width = "15rem";
     document.getElementById("btnSideBar").classList.remove("sideBarFechada");
     document.getElementById("btnSideBar").classList.add("sideBarAberta");  
   }
   else{
     document.getElementById("conteudoSideBar").style.width = "0";
-    document.getElementById("corpoSite").style.marginLeft = "0";
     document.getElementById("btnSideBar").classList.remove("sideBarAberta");
     document.getElementById("btnSideBar").classList.add("sideBarFechada");  
   }
