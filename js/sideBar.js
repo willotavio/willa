a799a2c167332c0b653b5a961c6faa4ef128fc93
willotavio <willaotavio@gmail.com>
2022-11-13 16:24:51 -0300

    Sidebar fechando com clique na página

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,14 +1,21 @@
 const botaoSideBar = document.querySelector(".botaoSideBar");
-
-botaoSideBar.addEventListener("click", function(){
-  if(botaoSideBar.classList.contains('sideBarFechada')){
+botaoSideBar.onclick = function(event){
+  if (event.target = botaoSideBar && botaoSideBar.classList.contains('sideBarFechada')){
     document.getElementById("conteudoSideBar").style.width = "15rem";
     document.getElementById("btnSideBar").classList.remove("sideBarFechada");
-    document.getElementById("btnSideBar").classList.add("sideBarAberta");  
+    document.getElementById("btnSideBar").classList.add("sideBarAberta");
   }
-  else{
+  else if (event.target = botaoSideBar && botaoSideBar.classList.contains('sideBarAberta')){
     document.getElementById("conteudoSideBar").style.width = "0";
     document.getElementById("btnSideBar").classList.remove("sideBarAberta");
-    document.getElementById("btnSideBar").classList.add("sideBarFechada");  
+    document.getElementById("btnSideBar").classList.add("sideBarFechada");
   }
-})
+}
+const conteudo = document.getElementById("divPerfil");
+conteudo.onclick = function(event){
+  if (event.target = conteudo && botaoSideBar.classList.contains('sideBarAberta')){
+    document.getElementById("conteudoSideBar").style.width = "0";
+    document.getElementById("btnSideBar").classList.remove("sideBarAberta");
+    document.getElementById("btnSideBar").classList.add("sideBarFechada");
+  }
+}
